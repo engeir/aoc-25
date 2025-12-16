32ffd19ad3a95caf523440888f479dea36090e63
engeir <38856990+engeir@users.noreply.github.com>
2025-12-16 18:46:40 +0100

    feat(day02): solve part 1

--- day02/main.go
+++ day02/main.go
@@ -3,34 +3,56 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/engeir/aoc-25/utils"
 )
 
 func main() {
-	// Test input
-	testLines, err := utils.ReadLines("test_input.txt")
+	lines, err := utils.ReadInput("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("=== Test Input ===")
-	fmt.Println("Part 1:", solvePart1(testLines))  // Should result in 1227775554
-	fmt.Println("Part 2:", solvePart2(testLines))
-
-	// Full input
-	lines, err := utils.ReadLines("./input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("=== Test Input ===")
+	fmt.Println("=== Full Input ===")
 	fmt.Println("Part 1:", solvePart1(lines))
 	fmt.Println("Part 2:", solvePart2(lines))
 }
 
-func solvePart1(lines []string) int {
+func checkRepetition(i int) int {
+	asString := strconv.Itoa(i)
+	length := len(asString)
+	if length%2 != 0 {
+		return 0
+	}
+	first := asString[:length/2]
+	second := asString[length/2:]
+	if first == second {
+		return i
+	}
 	return 0
 }
 
-func solvePart2(lines []string) int {
+func getIntRange(s string) int {
+	parts := strings.Split(s, "-")
+	p1, _ := strconv.Atoi(parts[0])
+	p2, _ := strconv.Atoi(parts[1])
+	count := 0
+	for i := p1; i < p2+1; i++ {
+		count += checkRepetition(i)
+	}
+	return count
+}
+
+func solvePart1(lines string) int {
+	sections := strings.Split(lines, ",")
+	count := 0
+	for _, s := range sections {
+		count += getIntRange(s)
+	}
+	return count
+}
+
+func solvePart2(lines string) int {
 	return 0
 }
---
+++ day02/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/engeir/aoc-25/utils"
+)
+
+func TestPart1(t *testing.T) {
+	lines, err := utils.ReadInput("./test_input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := solvePart1(lines)
+	want := 1227775554
+	if got != want {
+		t.Errorf("We got %d, wanted %d", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	// want := 0
+}
