0bb34e1c121f2a4a39af0413e99c4087062b07bc
engeir <38856990+engeir@users.noreply.github.com>
2025-12-20 10:07:19 +0100

    feat(day05): solve part 1
    We are now much smarter about how we construct and check the ranges.
    Instead of creating a map for all valid (fresh) ingredients, we just
    look at lower and upper bounds, and we narrow them down before ever
    touching the ingredients list.

--- day05/main.go
+++ day05/main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/engeir/aoc-25/utils"
 )
 
-func createFreshRanges(lines []string) (map[int]bool, int) {
-	m := make(map[int]bool)
+func createFreshRanges(lines []string) (map[int][2]int, int) {
+	m := make(map[int][2]int)
 	lineNumber := 0
 	for j, line := range lines {
 		if line == "" {
@@ -26,22 +26,83 @@ func createFreshRanges(lines []string) (map[int]bool, int) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		for i := start; i < stop+1; i++ {
-			m[i] = true
+		if len(m) == 0 {
+			tmp := [2]int{start, stop}
+			m[0] = tmp
+			continue
+		}
+		for k, v := range m {
+			switch {
+			case start <= v[0] && stop > v[0] && stop < v[1]:
+				tmp := [2]int(m[k])
+				tmp[0] = start
+				m[k] = tmp
+			case start <= v[0] && stop >= v[1]:
+				tmp := [2]int(m[k])
+				tmp[0] = start
+				tmp[1] = stop
+				m[k] = tmp
+			case start > v[0] && stop >= v[1] && start < v[1]:
+				tmp := [2]int(m[k])
+				tmp[1] = stop
+				m[k] = tmp
+			case start > v[0] && stop < v[1]:
+			default:
+				tmp := [2]int{start, stop}
+				m[j] = tmp
+			}
 		}
 	}
 	return m, lineNumber
 }
 
-func countFreshIngredients(lines []string, lineNumber int, fresh map[int]bool) int {
+func concatenateRanges(ranges map[int][2]int) (map[int][2]int, int) {
+	count := 0
+	for j, startStop := range ranges {
+		for k, v := range ranges {
+			if j == k {
+				continue
+			}
+			switch {
+			case startStop[0] <= v[0] && startStop[1] > v[0] && startStop[1] < v[1]:
+				tmp := [2]int(ranges[k])
+				tmp[0] = startStop[0]
+				ranges[k] = tmp
+				delete(ranges, j)
+				count++
+			case startStop[0] <= v[0] && startStop[1] >= v[1]:
+				tmp := [2]int(ranges[k])
+				tmp[0] = startStop[0]
+				tmp[1] = startStop[1]
+				ranges[k] = tmp
+				delete(ranges, j)
+				count++
+			case startStop[0] > v[0] && startStop[1] >= v[1] && startStop[0] < v[1]:
+				tmp := [2]int(ranges[k])
+				tmp[1] = startStop[1]
+				ranges[k] = tmp
+				delete(ranges, j)
+				count++
+			case startStop[0] > v[0] && startStop[1] < v[1]:
+				delete(ranges, j)
+				count++
+			}
+		}
+	}
+	return ranges, count
+}
+
+func countFreshIngredients(lines []string, lineNumber int, fresh map[int][2]int) int {
 	count := 0
 	for _, line := range lines[lineNumber:] {
 		result, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if fresh[result] {
-			count++
+		for _, v := range fresh {
+			if result > v[0] && result <= v[1] {
+				count++
+			}
 		}
 	}
 	return count
@@ -49,6 +110,13 @@ func countFreshIngredients(lines []string, lineNumber int, fresh map[int]bool) i
 
 func solvePart1(lines []string) int {
 	m, lineNumber := createFreshRanges(lines)
+	c := 0
+	for {
+		m, c = concatenateRanges(m) 
+		if c == 0 {
+			break
+		}
+	}
 	return countFreshIngredients(lines, lineNumber, m)
 }
 
