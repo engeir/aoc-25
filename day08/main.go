5d9bb3c257db69660aac42174c0a0d8ed8c84574
engeir <38856990+engeir@users.noreply.github.com>
2026-01-04 00:16:54 +0100

    feat(day08): parse all lines to coord at once
    Now, we got from about 500 to about 400 seconds.

--- day08/main.go
+++ day08/main.go
@@ -12,54 +12,35 @@ import (
 	"github.com/engeir/aoc-25/utils"
 )
 
-func extractNumbers(pos string) ([]int, error) {
+type Coord struct {
+	X, Y, Z int
+}
+
+func parseCoord(pos string) (Coord, error) {
 	posIdx := strings.Split(pos, ",")
 	posX, err := strconv.Atoi(posIdx[0])
 	if err != nil {
-		return []int{}, err
+		return Coord{}, err
 	}
 	posY, err := strconv.Atoi(posIdx[1])
 	if err != nil {
-		return []int{}, err
+		return Coord{}, err
 	}
 	posZ, err := strconv.Atoi(posIdx[2])
 	if err != nil {
-		return []int{}, err
+		return Coord{}, err
 	}
-	return []int{posX, posY, posZ}, nil
+	return Coord{posX, posY, posZ}, nil
 }
 
-func calcDistance(pos1, pos2 string) float64 {
-	pos1Idx, err := extractNumbers(pos1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pos2Idx, err := extractNumbers(pos2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	posX := pos2Idx[0] - pos1Idx[0]
-	posY := pos2Idx[1] - pos1Idx[1]
-	posZ := pos2Idx[2] - pos1Idx[2]
+func calcDistance(pos1, pos2 Coord) float64 {
+	posX := pos2.X - pos1.X
+	posY := pos2.Y - pos1.Y
+	posZ := pos2.Z - pos1.Z
 	return math.Sqrt(math.Pow(float64(posX), 2) + math.Pow(float64(posY), 2) + math.Pow(float64(posZ), 2))
 }
 
-func findClosestPair(lines []string) (float64, [2]string) {
-	shortestDistance := math.MaxFloat64
-	var shortestDistancePair [2]string
-	for idx, current := range lines {
-		for _, line := range lines[idx+1:] {
-			dist := calcDistance(current, line)
-			if dist < shortestDistance {
-				shortestDistance = dist
-				shortestDistancePair = [2]string{current, line}
-			}
-		}
-	}
-	return shortestDistance, shortestDistancePair
-}
-
-func RemoveIndex(s [][]string, index int) [][]string {
+func RemoveIndex(s [][]Coord, index int) [][]Coord {
 	return append(s[:index], s[index+1:]...)
 }
 
@@ -82,7 +63,7 @@ func uniqueSlice(slice []BoolFloatTuple) []BoolFloatTuple {
 	return result
 }
 
-func connectClosestCircuits(allCircs [][]string, nth int) [][]string {
+func connectClosestCircuits(allCircs [][]Coord, nth int) [][]Coord {
 	shortestDistance := math.MaxFloat64
 	localDistance := math.MaxFloat64
 	var (
@@ -123,14 +104,22 @@ func connectClosestCircuits(allCircs [][]string, nth int) [][]string {
 }
 
 func solvePart1(lines []string) int {
-	var circuits [][]string
+	var coords []Coord
 	for _, line := range lines {
-		circuits = append(circuits, []string{line})
+		parsed, err := parseCoord(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		coords = append(coords, parsed)
+	}
+	var circuits [][]Coord
+	for _, coord := range coords {
+		circuits = append(circuits, []Coord{coord})
 	}
 	for nth := 1; nth <= 1000; nth++ {
 		circuits = connectClosestCircuits(circuits, nth)
 	}
-	slices.SortStableFunc(circuits, func(a, b []string) int { return cmp.Compare(len(b), len(a)) })
+	slices.SortStableFunc(circuits, func(a, b []Coord) int { return cmp.Compare(len(b), len(a)) })
 	result := 1
 	lastLenght := 0
 	i := 0
